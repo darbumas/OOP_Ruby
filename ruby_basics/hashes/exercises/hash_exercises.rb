0b37c8a14b40496a9dad932e0fe6154d40f9d7d7
darbumas <2314@holbertonstudents.com>
2023-09-13 03:46:09 -0500

    remove white spaces

--- ruby_basics/hashes/exercises/hash_exercises.rb
+++ ruby_basics/hashes/exercises/hash_exercises.rb
@@ -20,7 +20,7 @@ end
 
 def update_favorite_movie(favorite_list, movie)
     # Step 1: add/update the key of movie (as a symbol)
-favorite_list[:movie] = movie 
+favorite_list[:movie] = movie
 
     # Step 2: return the hash (because Step 1 returns the value of the movie key)
     favorite_list
@@ -36,12 +36,12 @@ end
 
 def favorite_categories(favorite_list)
     # return the keys of favorite_list
-    favorite_list.keys 
+    favorite_list.keys
 end
 
 def favorite_items(favorite_list)
     # return the values of favorite_list
-    favorite_list.values  
+    favorite_list.values
 end
 
 def merge_favorites(original_list, additional_list)
